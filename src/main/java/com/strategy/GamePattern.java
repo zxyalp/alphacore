872c8eb3a0ba2b1cc3dc35b0da9517cf7356a197
醒言 <orcexzy@163.com>
2017-07-06 00:02:48 +0800

    add dark class

--- src/main/java/com/strategy/GamePattern.java
+++
@@ -1,7 +0,0 @@
-package com.strategy;
-
-/**
- * Created by summe on 2017/7/3.
- */
-public class GamePattern {
-}
---
+++ src/main/java/com/strategy/GamePatternDemo.java
@@ -0,0 +1,29 @@
+package com.strategy;
+
+/**设计模式一、策略模式
+ *
+ * Created by summe on 2017/7/3.
+ */
+
+abstract class Dark{
+
+    public void quack(){
+        System.out.print("gua,gua...");
+    }
+
+    public void swim(){
+        System.out.print("be able to swimming...");
+    }
+
+    public abstract void display();
+
+
+}
+
+
+
+
+
+
+public class GamePatternDemo {
+}
